ff6740cf0d7634fdab8b4a75670213ac928c5100
L3onnardd <bsantiagosgalicia@gmail.com>
2025-10-25 12:33:28 -0600

    cambios en Frorntend proyecto QR

--- Frontend/ProyectoQRv1/src/app/Vista/inicio/inicio.ts
+++ Frontend/ProyectoQRv1/src/app/Vista/inicio/inicio.ts
@@ -1,36 +1,33 @@
 import { Component, signal, effect } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { RouterLinkWithHref } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
   imports: [
-    MatButtonModule,
-    MatCardModule,
-    //RouterLinkWithHref, // Descomentar si se usa en la plantilla
     CommonModule
   ],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css'
 })
-export class Inicio {
+export class Inicio { 
+  
+  // Lógica del carrusel (Se mantiene tu arreglo de imágenes)
   imagenes = [
     'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhsLYhNpGPpeu5RTOh8VL9EggGnee6fgW5QvxHN9FVFUnYqYF-IU1w4SHIHV_yUvLMopgua1YoYiyidZ9S0lvABoK8VBMYS3WuqWZoZPt0ZJ-0mQtnQYBqCo4_0QvV6yEQ7OsIdkpKpEC_K/s1600/DJI_0040.jpg',
     'https://uregional.online/pluginfile.php/1/theme_academi/mspotmedia/1739474848/Logo%202.jpg',
+      'assets/img/alumnos.jpg',
+      'assets/img/alumnos2.jpg',
+      'assets/img/asistencia.jpg'
   ];
-
   imagenActual = signal(0);
 
   constructor() {
+    // Efecto del carrusel
     effect(() => {
       setInterval(() => {
         this.imagenActual.update(i => (i + 1) % this.imagenes.length);
-      }, 4000); // cambia cada 4 segundos
+      }, 4000); 
     });
   }
 }
-
-
